ff11fa87518ebd2be902137d0453b3dd7a91fa81
Kiyingi Timothy <kiyingitimo@gmail.com>
2024-08-31 20:56:04 +0300

    Add coments to backend code

--- Club Vault/app.py
+++ Club Vault/app.py
@@ -6,12 +6,19 @@ import time
 from database import Vault, FileData, db_start, Session
 from playhouse.shortcuts import model_to_dict
 app = flask.Flask(__name__)
+# Returns the index page (the one with the open vault)
 @app.route('/')
 def index():
     return flask.render_template('index.html')
+
+# Returns the create page (where one can create a vault)
 @app.route('/create')
 def create():
     return flask.render_template('create.html')
+
+# Returns the dashboard page (where one can view files, download and upload)
+# It also checks if it is from the create.html so that it can show a helpful message.
+# This message will inform the user that their vault ID must be memorised or written down.
 @app.route('/dash')
 def dash():
     has_create = ""
@@ -19,6 +26,9 @@ def dash():
         has_create = '1'
     return flask.render_template('dash.html', from_create=has_create)
 
+# This creates new vaults when given the name of the vault and the passcode
+# This does not use password hashing since it is a waste of time
+# In addition to that, it also create an initial session for the user to upload files
 @app.route('/create_handler', methods=["POST"])
 def create_handler():
     data = flask.request.get_json()
@@ -28,32 +38,41 @@ def create_handler():
     session.save()
     return {'session_id':session.ID, 'name': data['name'], 'vault_id': vault.ID}
 
+# This handles file uploads to the server
 @app.route('/upload_handler', methods=["POST"])
 def upload_handler():
     data = flask.request.form
-    session = Session.get_or_none(Session.ID == data['session'])
+    session = Session.get_or_none(Session.ID == data['session']) # checks the session in the database
     if not session:
         return {}, 404
-    filename = str(int(time.time()))
-    filedata = FileData.create(oname=data['oname'], filename=filename, filesize=data['size'], when=data['created'], vault=session.vault)
+    filename = str(int(time.time())) # The filename for these files is the timestamp of upload
+    filedata = FileData.create(
+        oname=data['oname'],
+        filename=filename,
+        filesize=data['size'],
+        when=data['created'],
+        vault=session.vault
+    ) # Create a dataentry in the database for this file inorder to track it
     filedata.save()
     file = flask.request.files['file']
-    file.save(os.path.join('user_files', filename))
+    file.save(os.path.join('user_files', filename)) # Then store the file data in a file with the respective filename
     return flask.redirect('/dash'), 200
 
+# Handler for the download button on the files
 @app.route('/download')
 def download():
     data = flask.request.args
-    session = Session.get_or_none(Session.ID == data['session_id'])
+    session = Session.get_or_none(Session.ID == data['session_id']) # Get session
     if not session:
         return {}, 404
-    file = FileData.get_or_none(FileData.filename == data['filename'])
+    file = FileData.get_or_none(FileData.filename == data['filename']) # get the file
     if not file:
         return {}, 418
     if file.vault != session.vault:
         return {}, 406
-    return flask.send_file(open(f"user_files/{file.filename}", 'rb'), download_name=file.oname)
+    return flask.send_file(open(f"user_files/{file.filename}", 'rb'), download_name=file.oname) # Send the file
 
+# This one deletes files
 @app.route('/delete')
 def delete():
     data = flask.request.args
@@ -66,11 +85,12 @@ def delete():
     if file.vault != session.vault:
         return {}, 406
     filepath = os.path.join('user_files', file.filename)
-    if os.path.exists(filepath):
-        os.remove(filepath)
-        file.delete_instance()
+    if os.path.exists(filepath): # Check file if exists
+        os.remove(filepath) # Delete the file
+        file.delete_instance() # Delete the file metadata
     return flask.redirect('/dash'), 200
 
+# This function runs when the user opens the dashboard and needs their data
 @app.route('/get_files')
 def get_files():
     data = flask.request.args
@@ -80,7 +100,7 @@ def get_files():
     files = session.vault.files
     nfiles = []
     for file in files:
-        nfiles.append(model_to_dict(file))
+        nfiles.append(model_to_dict(file)) # converts the tables to json
     return {
         "files": nfiles,
     }
--- Club Vault/database.py
+++ Club Vault/database.py
@@ -1,26 +1,31 @@
 from peewee import * # type: ignore
 
+# For this file I don't need to be too detailed since it's a database
+# So I'm just going to write what the fields and models mean
 db = SqliteDatabase('test.db')
 
-class BaseModel(Model):
+class BaseModel(Model): # This just simplifies life for me
     class Meta:
         database = db
 
+# Stores data about the Vault
 class Vault(BaseModel):
-    ID = AutoField(primary_key=True)
-    name = CharField(20)
-    passcode = CharField(6)
+    ID = AutoField(primary_key=True) # Unique ID
+    name = CharField(20) # name of the vault
+    passcode = CharField(6) # passcode of the vault
 
+# This is the file metadata. It is used to identify files.
 class FileData(BaseModel):
-    oname = CharField(100)
-    filename = CharField(12)
-    filesize = FloatField()
-    when = FloatField()
-    vault = ForeignKeyField(Vault, backref="files")
+    oname = CharField(100) # The file's original name
+    filename = CharField(12) # The file name assigned to it ( It's upload timestamp ) PS: It is unique since we don't time travel
+    filesize = FloatField() # The size of the file
+    when = FloatField() # Timestamp of when upload
+    vault = ForeignKeyField(Vault, backref="files") # Vault it belongs to
 
+# This stores user sessions on the server
 class Session(BaseModel):
-    ID = AutoField(primary_key=True)
-    vault = ForeignKeyField(Vault, backref='sessions')
+    ID = AutoField(primary_key=True) # Unique ID of the session
+    vault = ForeignKeyField(Vault, backref='sessions') # The Vault it's linked to
 
 def db_start():
     db.connect()
